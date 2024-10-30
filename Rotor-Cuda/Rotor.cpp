4bb8df23d9a3306e4a5289b747530e0a9d1cd944
Crow1994 <60434587+Crow1994@users.noreply.github.com>
2024-10-30 14:16:59 +0400

    Update Rotor.cpp

--- Rotor-Cuda/Rotor.cpp
+++ Rotor-Cuda/Rotor.cpp
@@ -1082,7 +1082,7 @@ void Rotor::FindKeyGPU(TH_PARAM * ph)
 	ph->rKeyRequest = false;
 
 
-	const int JUMP_INTERVAL_SECONDS = 20; // Adjust as needed
+	const int JUMP_INTERVAL_SECONDS = 5; // Adjust as needed
 
 	// Start the timer
 	auto lastJumpTime = std::chrono::high_resolution_clock::now();
